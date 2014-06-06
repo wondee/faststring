fc828b857067df0fdd62fd6d6cd069117c20a4b3
markus <wondee@github.com>
2014-06-06 09:39:45 +0200

    finalized ParserTest method

--- analysis-test/src/main/java/substring/ParserTest.java
+++ analysis-test/src/main/java/substring/ParserTest.java
@@ -6,13 +6,18 @@ public class ParserTest {
 		new ParserTest().parse("abababababab");
 	}
 	
-	public void parse(String line) {
+	public String parse(String line) {
+		
+		String result = "";
 		
 		for (int i = 0; i < 10; i+=2) {
 			String substring = line.substring(i , 2);
 			
+			
+			result += substring;
 		}
 		
+		return result;
 	}
 	
 }
