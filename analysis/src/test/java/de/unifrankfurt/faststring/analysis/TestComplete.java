7a6142a4a6b69cdf59398afc1214574b849a2e47
markus <wondee@github.com>
2014-06-06 12:02:07 +0200

    added (a should be) integration test

---
+++ analysis/src/test/java/de/unifrankfurt/faststring/analysis/TestComplete.java
@@ -0,0 +1,17 @@
+package de.unifrankfurt.faststring.analysis;
+
+import org.junit.Test;
+
+public class TestComplete extends BaseAnalysisTest{
+
+	
+	@Test
+	public void testParserTest() {
+		
+		
+		SubstringAnalyzer analyzer = new SubstringAnalyzer(getTargetApplication(), getMethod("ParserTest", "parse"));
+		
+		analyzer.findCandidates();
+	}
+
+}
