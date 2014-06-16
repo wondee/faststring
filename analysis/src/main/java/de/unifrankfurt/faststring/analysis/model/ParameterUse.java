273db760f28c52724cd89405b7aaec197420aa0c
cb2woak <cb2woak@AP551938.zitd.global.commerzbank.com>
2014-06-16 15:15:57 +0200

    removed TODO marker

--- analysis/src/main/java/de/unifrankfurt/faststring/analysis/model/ParameterUse.java
+++ analysis/src/main/java/de/unifrankfurt/faststring/analysis/model/ParameterUse.java
@@ -27,9 +27,6 @@ public class ParameterUse extends Use {
 
 	@Override
 	public List<Integer> getConnectedRefs(TypeLabel label) {
-		// TODO check more precise in which cases the return value chould be labeled
-		// in the actual error String.valueOf(labeledItem) does not return a labeled item but a normal String
-		
 		if (label.canReturnedValueBeLabeled(method)) {
 			return Lists.newArrayList(def);			
 		} else {
