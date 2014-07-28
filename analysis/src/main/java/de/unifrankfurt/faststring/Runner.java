dda57fd275a706f00e42e53dcc35ccf1017b6821
markus <wondee@github.com>
2014-07-28 07:16:31 +0200

    added error handling

--- analysis/src/main/java/de/unifrankfurt/faststring/Runner.java
+++ analysis/src/main/java/de/unifrankfurt/faststring/Runner.java
@@ -1,6 +1,8 @@
 package de.unifrankfurt.faststring;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -15,6 +17,7 @@ import com.beust.jcommander.IStringConverter;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.google.common.base.Predicates;
+import com.google.common.base.Splitter;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
@@ -32,13 +35,10 @@ import de.unifrankfurt.faststring.transform.JarManager;
 public class Runner {
 	private static final Logger LOG = LoggerFactory.getLogger(Runner.class);
 
-	@Parameter(names = "-labels",
-			description = "The optimizing labels to be used",
-			required=true,
-			converter=TypeLabelCreator.class)
+	@Parameter(names = "-labels", description = "The optimizing labels to be used", required = true, converter = TypeLabelCreator.class)
 	private List<TypeLabel> typeList;
 
-	@Parameter(names = "-jar", required=true)
+	@Parameter(names = "-jar", required = true)
 	private String jarFile;
 
 	@Parameter(names = "-exclusion")
@@ -60,31 +60,15 @@ public class Runner {
 		}
 
 		try {
-			TargetApplication targetApplication = new TargetApplication(jarFile, exclusionFile);
+			TargetApplication targetApplication = new TargetApplication(
+					jarFile, exclusionFile);
 
 			Map<String, AnalysisResult> analysisResult = Maps.newTreeMap();
 
 			for (IClass clazz : targetApplication.getApplicationClasses()) {
-
-				LOG.info("analyzing class: {}", clazz.getName());
-				for (IMethod m : clazz.getDeclaredMethods()) {
-					AnalyzedMethod method = targetApplication.findIRMethodForMethod(m);
-
-					if (method != null) {
-						MethodAnalyzer analyzer = new MethodAnalyzer(method, filteredList);
-
-						AnalysisResult candidates = analyzer.analyze();
-
-						if (!candidates.isEmpty()) {
-							analysisResult.put(m.getSignature(), candidates);
-						}
-					} else {
-						LOG.error("method is skipped");
-					}
-
-				}
-
+				processClass(filteredList, targetApplication, analysisResult, clazz);
 			}
+
 			LOG.info("creating outputfile");
 
 			Path outputPath = Paths.get("faststring-output/");
@@ -107,6 +91,55 @@ public class Runner {
 
 	}
 
+	private void processClass(List<TypeLabel> filteredList,
+			TargetApplication targetApplication,
+			Map<String, AnalysisResult> analysisResult, IClass clazz) {
+		LOG.info("analyzing class: {}", clazz.getName());
+		for (IMethod m : clazz.getDeclaredMethods()) {
+			try {
+				AnalyzedMethod method = targetApplication.findIRMethodForMethod(m);
+
+				if (method != null) {
+					MethodAnalyzer analyzer = new MethodAnalyzer(method, filteredList);
+
+					AnalysisResult candidates = analyzer.analyze();
+
+					if (!candidates.isEmpty()) {
+						analysisResult.put(m.getSignature(), candidates);
+					}
+				} else {
+					LOG.error("method is skipped");
+				}
+			} catch (Exception e) {
+				handleError(e, m);
+			}
+
+		}
+	}
+
+	private void handleError(Exception e, IMethod m) {
+		try {
+			LOG.error("method is skipped, because of error", e);
+
+			Path errorPath = Paths.get("faststring-errors/");
+			if (!Files.exists(errorPath)) {
+				Files.createDirectory(errorPath);
+			}
+
+			Path errorFile = Paths.get("faststring-errors/", m.getSignature().replace('/', '.'));
+
+			StringWriter stringWriter = new StringWriter();
+			PrintWriter writer = new PrintWriter(stringWriter);
+			e.printStackTrace(writer);
+
+			Iterable<String> stackTrace = Splitter.on(System.getProperty("line.separator")).split(stringWriter.toString());
+
+			Files.write(errorFile, stackTrace, Charset.defaultCharset());
+
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+	}
 
 	public static class TypeLabelCreator implements IStringConverter<TypeLabel> {
 
@@ -117,17 +150,15 @@ public class Runner {
 				Class<?> c = Class.forName(string);
 
 				if (TypeLabel.class.isAssignableFrom(c)) {
-
 					LOG.info("using typelabel: {}", c.getName());
-
 					TypeLabel label = (TypeLabel) c.newInstance();
 
 					return label;
-
 				} else {
-					LOG.error("class is not implementing the TypeLabel interface: {}", c.getName());
+					LOG.error(
+							"class is not implementing the TypeLabel interface: {}",
+							c.getName());
 				}
-
 
 			} catch (ClassNotFoundException e) {
 				LOG.error("could not load type label", string, e);
